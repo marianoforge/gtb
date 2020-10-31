b3ac30477f28588beb97618e01f6d4f5f0cb04cb
Victor Antolini <victor.antolini@forgedev.co>
2020-10-31 20:38:16 -0300

    updates

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -24,7 +24,7 @@ const Layout = () => {
             </Link>       
 
             <div className="flex flex-grow justify-end">
-              <button id="hamburgerbtn" onClick={mobileClick} className="lg:hidden text-white h-6 p-4 flex justify-center items-center border-0 relative z-10">
+              <button aria-label="Abrir el menu" id="hamburgerbtn" onClick={mobileClick} className="lg:hidden text-white h-6 p-4 flex justify-center items-center border-0 relative z-10">
                 <svg className="w-6 h-6 icn icn-hamb" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16">
